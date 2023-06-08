60787fcef128f5b0588bc6c2e2ab2de1bbe03b48
Eddie Abramov <ediabc11@gmail.com>
2023-06-08 19:36:46 +0300

    add test to competitionController

--- src/main/java/com/example/sport_api/controllers/CompetitionController.java
+++ src/main/java/com/example/sport_api/controllers/CompetitionController.java
@@ -1,6 +1,5 @@
 package com.example.sport_api.controllers;
 
-import org.modelmapper.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.CacheControl;
@@ -9,32 +8,21 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.sport_api.exceptions.NotFoundException;
 import com.example.sport_api.models.Competition;
 import com.example.sport_api.models.CompetitionDto;
-import com.example.sport_api.repositories.CompetitionRepository;
 import com.example.sport_api.services.CompetitionService;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-public class CompetitionContrller {
-
-    @Autowired
-    private CompetitionRepository competitionRepository;
+public class CompetitionController {
 
     @Autowired
     private CompetitionService competitionService;
 
-    public CompetitionContrller(CompetitionRepository competitionRepository) {
-        this.competitionRepository = competitionRepository;
-    }
-
-    @RequestMapping("/com")
+    @GetMapping("/com")
     public List<CompetitionDto> retrieveAllTeams() {
         return competitionService.getAllCompetitions();
     }
@@ -59,11 +47,6 @@ public class CompetitionContrller {
                     .body(null);
         } catch (DataAccessException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .cacheControl(CacheControl.noCache())
-                    .body(null);
-        } catch (ValidationException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .contentType(MediaType.APPLICATION_JSON)
                     .cacheControl(CacheControl.noCache())
                     .body(null);
--- src/main/java/com/example/sport_api/exceptions/AreaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.sport_api.exceptions;
-
-public class AreaNotFoundException extends RuntimeException {
-
-    public AreaNotFoundException(String message) {
-        super(message);
-    }
-}
